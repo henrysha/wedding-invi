27645a7c9eb4872aaabb1e5db807e02bf34658c9
Henry Seongwook Ha <swh0901@gmail.com>
2022-12-14 23:59:24 +0900

    bold

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function InviPage() {
       ></iframe>
       <Link
         href="/attendance"
-        className="text-bold flex h-16 w-screen place-items-center justify-center bg-fuchsia-300 text-xl text-white"
+        className="flex h-16 w-screen place-items-center justify-center bg-fuchsia-300 text-xl font-bold text-white"
       >
         참석 여부 전달하기
       </Link>
