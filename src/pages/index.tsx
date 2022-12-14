f96b10cc29cdbdad3712987fbee10d597e276255
Henry Seongwook Ha <swh0901@gmail.com>
2022-12-15 00:49:38 +0900

    move kakaotalk sharing button to bottom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,20 +9,20 @@ export default function InviPage() {
       <Head>
         <title>모바일 청첩장</title>
       </Head>
-      <div className="relative h-full">
+      <div className="h-full">
         <iframe
           className="fill-with-bottom-margin"
           src={env.NEXT_PUBLIC_INVITATION_LINK}
         ></iframe>
-        <div className="absolute right-4 bottom-20">
+        <div className="flex items-center">
+          <Link
+            href="/attendance"
+            className="flex h-16 flex-grow place-items-center justify-center bg-fuchsia-300 text-xl font-bold text-white"
+          >
+            참석 여부 전달하기
+          </Link>
           <KakaoCustomShareButton />
         </div>
-        <Link
-          href="/attendance"
-          className="flex h-16 w-screen place-items-center justify-center bg-fuchsia-300 text-xl font-bold text-white"
-        >
-          참석 여부 전달하기
-        </Link>
       </div>
     </>
   )
